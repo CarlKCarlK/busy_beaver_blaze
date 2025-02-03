fe948b9348564fe5138749b509c0f6eb72654257
Carl Kadie <carlk@msn.com>
2025-02-02 16:49:12 -0800

    host on github

---
+++ docs/pkg/busy_beaver_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_machine_free: (a: number, b: number) => void;
+export const machine_from_string: (a: number, b: number) => [number, number, number];
+export const machine_step: (a: number) => number;
+export const machine_space_time_js: (a: number, b: number, c: number, d: number) => [number, number];
+export const machine_count_js: (a: number, b: number) => number;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
