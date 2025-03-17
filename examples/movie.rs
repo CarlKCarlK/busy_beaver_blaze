da3aae4e0be93ab23940050319767b2e3b93919e
Carl Kadie <carlk@msn.com>
2025-03-17 15:12:02 -0700

    add support for bb6_contender10T with 10 trillion value

--- examples/movie.rs
+++ examples/movie.rs
@@ -79,6 +79,10 @@ fn main() -> Result<(), Box<dyn core::error::Error>> {
             let dir_info = create_sequential_subdir(r"m:\deldir\bb\bb6_contender")?;
             (BB6_CONTENDER, 1_000_000_000_000u64, 2000, dir_info)
         }
+        "bb6_contender10T" => {
+            let dir_info = create_sequential_subdir(r"m:\deldir\bb\bb6_contender")?;
+            (BB6_CONTENDER, 10_000_000_000_000u64, 2000, dir_info)
+        }
         "bb6_contender2" => {
             let dir_info = create_sequential_subdir(r"m:\deldir\bb\bb6_contender2")?;
             (BB6_CONTENDER, 1_000_000_000u64, 1000, dir_info)
